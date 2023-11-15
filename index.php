ac7eb9ff37e01eeda820ee4e45d335fd22ef070d
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-15 11:27:29 +0100

    fin du crud 1

--- index.php
+++ index.php
@@ -27,11 +27,18 @@ include 'query.php';
         <p><?php DisplayAllCustomerWithCard(); ?></p>
         <hr>
         <h2>Exercice 5</h2>
+        <h3>Afficher uniquement le nom et le prénom de tous les clients dont le nom commence par la lettre "M". Les afficher comme ceci :</h3>
+        <p><?php DisplayAllCustomerStartByM(); ?></p>
+        <hr>
+        <h2>Exercice 6</h2>
         <h3>Afficher le titre de tous les spectacles ainsi que l'artiste, la date et l'heure. Trier les titres par ordre alphabétique. Afficher les résultat comme ceci : Spectacle par artiste, le date à heure.</h3>
         <p><?php DisplayAllShowWithArtistDateHour(); ?></p>
         <hr>
-        <h2>Exercice 6</h2>
-        
+        <h2>Exercice 7</h2>
+        <h3>display all client in a card format with fidelity card y/n then his card number</h3>
+        <p><?php displayClientCard(); ?></p>
+
+
 
 
     </body>
--- query.php
+++ query.php
@@ -1,83 +1,121 @@
 <?php
 // Display all customer
-function DisplayAllCustomer(){
+function DisplayAllCustomer()
+{
     require 'connect.php';
-    $query=$pdo->query('SELECT * FROM clients');
-    $clients=$query->fetchAll();
+    $query = $pdo->query('SELECT * FROM clients');
+    $clients = $query->fetchAll();
     echo "<table>";
     echo "<tr><th>Last Name</th><th>First Name</th></tr>";
-    foreach ($clients as $client){
+    foreach ($clients as $client) {
         echo "<tr>";
-        echo "<td>".$client['lastName']."</td>";
-        echo "<td>".$client['firstName']."</td>";
+        echo "<td>" . $client['lastName'] . "</td>";
+        echo "<td>" . $client['firstName'] . "</td>";
         echo "</tr>";
     }
     echo "</table>";
 }
 
 // Display all show
-function DisplayAllShow(){
-require 'connect.php';
-$query=$pdo->query('SELECT * FROM shows');
-$shows=$query->fetchAll();
-echo "<table>";
-echo "<tr><th>Title</th></tr>";
-foreach ($shows as $show){
-    echo "<tr>";
-    echo "<td>".$show['title']."</td>";
-    echo "</tr>";
-}
-echo "</table>";    
-
+function DisplayAllShow()
+{
+    require 'connect.php';
+    $query = $pdo->query('SELECT * FROM shows');
+    $shows = $query->fetchAll();
+    echo "<table>";
+    echo "<tr><th>Title</th></tr>";
+    foreach ($shows as $show) {
+        echo "<tr>";
+        echo "<td>" . $show['title'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
 }
 
 // Display 20 first customer
-function Display20Customer(){
+function Display20Customer()
+{
     require 'connect.php';
-    $query=$pdo->query('SELECT * FROM clients LIMIT 20');
-    $clients=$query->fetchAll();
+    $query = $pdo->query('SELECT * FROM clients LIMIT 20');
+    $clients = $query->fetchAll();
     echo "<table>";
     echo "<tr><th>Last Name</th><th>First Name</th></tr>";
-    foreach ($clients as $client){
+    foreach ($clients as $client) {
         echo "<tr>";
-        echo "<td>".$client['lastName']."</td>";
-        echo "<td>".$client['firstName']."</td>";
+        echo "<td>" . $client['lastName'] . "</td>";
+        echo "<td>" . $client['firstName'] . "</td>";
         echo "</tr>";
     }
     echo "</table>";
 }
 
 // Display all customer with card
-function DisplayAllCustomerWithCard(){
+function DisplayAllCustomerWithCard()
+{
     require 'connect.php';
-    $query=$pdo->query('SELECT * FROM clients WHERE card=1');
-    $clients=$query->fetchAll();
+    $query = $pdo->query('SELECT * FROM clients WHERE card=1');
+    $clients = $query->fetchAll();
     echo "<table>";
     echo "<tr><th>Last Name</th><th>First Name</th></tr>";
-    foreach ($clients as $client){
+    foreach ($clients as $client) {
         echo "<tr>";
-        echo "<td>".$client['lastName']."</td>";
-        echo "<td>".$client['firstName']."</td>";
+        echo "<td>" . $client['lastName'] . "</td>";
+        echo "<td>" . $client['firstName'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+// Display all customer start by M
+function DisplayAllCustomerStartByM()
+{
+    require 'connect.php';
+    $query = $pdo->query('SELECT * FROM clients WHERE lastName LIKE "M%" ORDER BY lastName');
+    $clients = $query->fetchAll();
+    echo "<table>";
+    echo "<tr><th>Last Name</th><th>First Name</th></tr>";
+    foreach ($clients as $client) {
+        echo "<tr>";
+        echo "<td>" . $client['lastName'] . "</td>";
+        echo "<td>" . $client['firstName'] . "</td>";
         echo "</tr>";
     }
     echo "</table>";
 }
 // Display all show with artist date and hour, order by title
 // display it like this: Spectacle by artist, date at hour
-function DisplayAllShowWithArtistDateHour(){
+function DisplayAllShowWithArtistDateHour()
+{
     require 'connect.php';
-    $query=$pdo->query('SELECT * FROM shows ORDER BY title');
-    $shows=$query->fetchAll();
+    $query = $pdo->query('SELECT * FROM shows ORDER BY title');
+    $shows = $query->fetchAll();
     echo "<table>";
     echo "<tr><th>Title</th><th>Artist</th><th>Date</th><th>Hour</th></tr>";
-    foreach ($shows as $show){
+    foreach ($shows as $show) {
         echo "<tr>";
-        echo "<td>".$show['title']."</td>";
-        echo "<td>".$show['performer']."</td>";
-        echo "<td>".$show['date']."</td>";
-        echo "<td>".$show['startTime']."</td>";
+        echo "<td>" . $show['title'] . "</td>";
+        echo "<td>" . $show['performer'] . "</td>";
+        echo "<td>" . $show['date'] . "</td>";
+        echo "<td>" . $show['startTime'] . "</td>";
         echo "</tr>";
     }
     echo "</table>";
 }
-?>
+// Display all client in a tile for each client display all show he has seen
+function displayClientCard()
+{
+    require 'connect.php';
+    $query = $pdo->query('SELECT * FROM clients');
+    $clients = $query->fetchAll();
+    foreach ($clients as $client) {
+        echo "<p> Nom: " . $client['lastName'] . "</p>";
+        echo "<p>Prénom: " . $client['firstName'] . "</p>";
+        echo "<p>Date de naissance: " . $client['birthDate'] . "</p>";
+        if ($client['card'] == 1) {
+            echo "<p>Carte de fidélité : Oui</p>";
+            echo "<p>Numéro de carte: " . $client['cardNumber'] . "</p>";
+        } else {
+            echo "<p>Carte de fidélité: Non</p>";
+        }
+        echo "<hr>";
+    }
+}
